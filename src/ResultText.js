ec39ca8fca1a012ceb3654e7bb997a30c5e26dec
Steven Sawtelle <steven@Stevens-MacBook-Air.local>
2022-02-12 18:06:26 -0700

    fix letter matching logic

--- src/ResultText.js
+++ src/ResultText.js
@@ -2,7 +2,68 @@ import React, { useState } from "react";
 
 import './styles/letters.scss';
 
+const getHitList = (guess, goal) => {
+
+    //first init array of incorrect guesses
+    let hitList = Array.apply(null, Array(guess.length)).map(() => 'incorrect' )
+    let usedLetters = {};
+    //first pass for correct letters
+    for(let i = 0; i < guess.length; i+=1){
+        if(i >= goal.length){
+            break;
+        }
+        if(goal[i] === guess[i]){
+            hitList[i] = 'correct';
+            if(usedLetters[goal[i]]){
+                usedLetters[goal[i]] += 1;
+            }
+            else {
+                usedLetters[goal[i]] = 1;
+            }
+        }
+    }
+    // console.log(hitList)
+    // console.log(usedLetters)
+    //then pass for misplaced letters
+    const goalCharArray = goal.split('');
+    // console.log(goalCharArray)
+    for(let i=0; i < guess.length; i+=1){
+        if(hitList[i]==='correct'){
+            continue;
+        }
+        let cur = guess[i];
+        if(goalCharArray.includes(cur)){
+            // console.log(`found for ${cur}`)
+            //possible misplace, but need to see if letter already used
+            const countOfCurInGoal = goal.split(cur).length - 1;
+            const alreadyUsedCurCount = usedLetters[cur] || 0;
+            // console.log(countOfCurInGoal)
+            // console.log(alreadyUsedCurCount)
+            if(countOfCurInGoal > alreadyUsedCurCount){
+                //misplaced
+                hitList[i] = 'misplaced';
+                if(usedLetters[cur]){
+                    usedLetters[cur] += 1;
+                }
+                else {
+                    usedLetters[cur] = 1;
+                }
+            }
+        }
+    }
+    // console.log(hitList)
+    // console.log(usedLetters)
+    return hitList;
+}
+
 const getStatus = (guess, goal, index) => {
+
+    //first pass for correct letters
+
+
+
+
+    console.log(guess, goal, index) 
     if(index >= goal.length){
         return goal.split('').includes(guess[index]) ? 'misplaced' : 'incorrect';
     }
@@ -21,12 +82,16 @@ const WordleLetter = ({ status, letter }) => {
 }
 
 const ResultText = ({guess, politician }) => {
+    const noSpaceGuess = guess.split(' ').join('');
+    const noSpacePolticianState = politician.state.toUpperCase().split(' ').join('');
+    const hitList = getHitList(noSpaceGuess,noSpacePolticianState);//eg ['correct', 'incorrect', 'incorrect', 'misplaced']
     return <div className='result-text'>
-        {guess.split('').map((letter, i) => {
-            return letter != ' ' ? <WordleLetter key={i}
-                status={getStatus(guess, politician.state.toUpperCase(), i)}
+        {noSpaceGuess.split('').map((letter, i) => {
+            return <WordleLetter key={i}
+                // status={getStatus(noSpaceGuess, noSpacePolticianState, i)}
+                status={hitList[i]}
                 letter={letter}
-            /> : <></>
+            />
         })}
     </div>
 }
